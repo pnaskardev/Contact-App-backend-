f1d7c9c5c11c35fe614bf8699eff8b0fb44513bd
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-13 23:02:04 +0530

    additional commit

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+// IMPORTS FROM PACKAGES
 const express=require('express');
 
+// IMPORTS FROM FILES
+const authRouter=require('./routes/auth');
 
+
+
+
+// INIT
+const PORT=3000;
 const app=express();
 
-app.listen(3000,"0.0.0.0");
+
+// MIDDLEWARES
+app.use(authRouter);
+
+app.listen(PORT,"0.0.0.0");
