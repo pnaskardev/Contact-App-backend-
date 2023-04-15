43394ebaf5d46f6d907313fa9fcf8954e1f7a2b0
pnaskardev <priyanshunaskar89@gmail.com>
2023-04-15 18:47:49 +0530

    further improvements

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ dotenv.config();
 const app = express();
 
 
-// CONNECTIONS
-mongoose.connect(process.env.DB_URI)
-.then(() => 
-{
-    console.log("Connection Succesful")
-})
-.catch(err => console.log(err));
-
 // MIDDLEWARES
 app.use(express.json());
 app.use('/auth', authRouter);
@@ -28,9 +20,14 @@ app.use('/contacts',contactRouter);
 app.use('/',(res,req,next)=>
 {
     console.log('Hello World');
-    res.statusCode(200);
 });
 
+// CONNECTIONS
+mongoose.connect(process.env.DB_URI)
+.then(() => 
+{
+    console.log("Connection Succesful");
+    app.listen(process.env.PORT, "0.0.0.0");
+})
+.catch(err => console.log(err));
 
-
-app.listen(process.env.PORT, "0.0.0.0");
